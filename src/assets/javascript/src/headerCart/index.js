aa8cd851dd9fa95773231de37bac2cc4efd3bfa1
kor1k <koortttt@gmail.com>
2020-08-09 14:40:03 +0300

    added header cart quantity circle

--- src/assets/javascript/src/headerCart/index.js
+++ src/assets/javascript/src/headerCart/index.js
@@ -1,3 +1,7 @@
-$("#cart").on("click", function() {
-    $(".shopping-cart").fadeToggle( "fast");
+let amountOfAddedItemsInCart = $('.shopping-cart-items .clearfix');
+console.log(amountOfAddedItemsInCart.length)
+$('#cartQuantityHeader').html(amountOfAddedItemsInCart.length)
+
+$("#cart").on("click", function () {
+    $(".shopping-cart").fadeToggle("fast");
 });
