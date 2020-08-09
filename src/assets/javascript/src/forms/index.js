ce7416501bcccd72d804bc76d948986114e473ef
kor1k <koortttt@gmail.com>
2020-08-10 00:16:47 +0300

    hidden form input check, styles for submit buttons

--- src/assets/javascript/src/forms/index.js
+++ src/assets/javascript/src/forms/index.js
@@ -1,7 +1,7 @@
 function setCookie(name, value, days) {
-    var expires = "";
+    let expires = "";
     if (days) {
-        var date = new Date();
+        let date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
@@ -9,10 +9,10 @@ function setCookie(name, value, days) {
 }
 
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
@@ -25,64 +25,73 @@ function eraseCookie(name) {
 
 $('#reg_submit').on('click', function () {
 
-    let user_data = $('#registerForm');
-    console.log(user_data);
+    if ($('#gender').val() === '') {
+        let user_data = $('#registerForm');
+        console.log(user_data);
 
-    let data = {
-        email: $('#reg_email').val(),
-        first_name: $('#reg_f_name').val(),
-        last_name: $('#reg_l_name').val(),
-        password: $('#reg_pass').val()
-    };
+        let data = {
+            email: $('#reg_email').val(),
+            first_name: $('#reg_f_name').val(),
+            last_name: $('#reg_l_name').val(),
+            password: $('#reg_pass').val()
+        };
 
-    const url = 'http://164.90.218.246:8000/auth';
+        const url = 'http://164.90.218.246:8000/auth';
 
-    $.ajax({
-        type: 'POST',
-        url: `${url}` + '/sign-up',
-        data: JSON.stringify(data),
-        dataType: "json",
-        beforeSend: function () {
-        },
-        success: function (response) {
-            alert('success success success success');
-            if (response.success) {
-                setCookie('logged_in', 'true', 0.5);
-            }
-        },
-    })
+        $.ajax({
+            type: 'POST',
+            url: `${url}` + '/sign-up',
+            data: JSON.stringify(data),
+            dataType: "json",
+            beforeSend: function () {
+                alert('beforeSend')
+            },
+            error: function (response) {
+                alert('error')
+            },
+            success: function (response) {
+                alert('success success success success');
+                if (response.success) {
+                    setCookie('logged_in', 'true', 0.5);
+                }
+            },
+        })
+    }
+
 });
 
 $('#login_submit').on('click', function () {
+    if ($('#gender').val() === '') {
+        let user_data = $('#loginForm');
+        console.log(user_data);
 
-    let user_data = $('#loginForm');
-    console.log(user_data);
+        let data = {
+            email: $('#login_email').val(),
+            password: $('#login_pass').val()
+        };
 
-    let data = {
-        email: $('#login_email').val(),
-        password: $('#login_pass').val()
-    };
+        const url = 'http://164.90.218.246:8000/auth';
 
-    const url = 'http://164.90.218.246:8000/auth';
+        $.ajax({
+            type: 'POST',
+            url: `${url}` + '/sign-in',
+            data: JSON.stringify(data),
+            dataType: "json",
+            beforeSend: function () {
+                alert('beforeSend')
+            },
+            error: function () {
+                alert('error error error error error');
+            },
+            success: function (response) {
+                alert('success success success success');
 
-    $.ajax({
-        type: 'POST',
-        url: `${url}` + '/sign-in',
-        data: JSON.stringify(data),
-        dataType: "json",
-        beforeSend: function () {
-        },
-        error: function () {
-            alert('error errorerrorerror');
-        },
-        success: function (response) {
-            alert('success success success success');
-
-            if (response.token) {
-                setCookie('logged_in', 'true', 0.5);
-            }
-        },
-    })
+                if (response.token) {
+                    setCookie('logged_in', 'true', 0.5);
+                }
+            },
+        })
+    }
 });
 
 const isUserLogged = () => {
