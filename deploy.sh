07124849e5de47cfac3af132ad4dfd9508c0a6a5
zhashkevych <zhashkevychmaksim@gmail.com>
2020-08-09 14:38:57 +0300

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build --no-cache . -t jewerly-frontent:0.1
 
-if [ "$(docker ps -q -f name=editt-client)" ]; then
+if [ "$(docker ps -q -f name=jewerly-frontend)" ]; then
     if [ ! "$(docker ps -aq -f status=exited -f name=jewerly-frontend)" ]; then
         docker stop jewerly-frontend
     fi
