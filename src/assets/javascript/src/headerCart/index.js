f9f1651c91968f5cdf5d24651e0f94d31974071a
kor1k <koortttt@gmail.com>
2020-08-09 15:55:33 +0300

    test get api

--- src/assets/javascript/src/headerCart/index.js
+++ src/assets/javascript/src/headerCart/index.js
@@ -1,7 +1,12 @@
 let amountOfAddedItemsInCart = $('.shopping-cart-items .clearfix');
-console.log(amountOfAddedItemsInCart.length)
-$('#cartQuantityHeader').html(amountOfAddedItemsInCart.length)
+if (amountOfAddedItemsInCart) {
+    $('#cartQuantityHeader').html(amountOfAddedItemsInCart.length);
+}
 
 $("#cart").on("click", function () {
     $(".shopping-cart").fadeToggle("fast");
+});
+
+$.getJSON('http://164.90.218.246:8000/admin/products', function (data) {
+    console.log(data)
 });
