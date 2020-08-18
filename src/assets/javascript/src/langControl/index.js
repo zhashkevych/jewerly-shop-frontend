56413337068678485e1cdfd0b0c1212a35f680be
kor1k <koortttt@gmail.com>
2020-08-18 23:20:17 +0300

    new langSwitcher

--- src/assets/javascript/src/langControl/index.js
+++ src/assets/javascript/src/langControl/index.js
@@ -1,67 +1,206 @@
 const langControl = () => {
-    let currLang = window.location.search.slice(-2);
+    $('#lang_en').hide();
 
-    if (currLang !== "") {
-        console.log('current lang code is: ' + currLang);
-        $('body').addClass('lang_' + currLang);
-        $('.current-lang .lang-text').html(currLang);
-        $(`.current-lang #lang_${currLang}`).html(currLang);
-        $(`.lang-dropdown #lang_${currLang}`).hide();
-    } else {
-        currLang = 'en';
-        console.log('current lang code is en')
-    }
+    $('.switch-lang').hover(function () {
+        $('.lang-dropdown').addClass('active');
+    });
 
+    $('.switch-lang').on('mouseleave', function () {
+        $('.lang-dropdown').removeClass('active');
+    });
 
-    let translations = {
-        en: {
-            headerTop: {
-                title: 'SilverRain Israel jewelry company since 2012. All silver products made in Italy',
-                login_title: 'Login'
-            },
-            categories: {
-                rings: 'Rings',
-                bracelets: 'Bracelets',
-                pendants: 'Pendants',
-                earrings: 'Earrings',
-                chokersNecklaces: 'Chokers / Necklaces',
-            },
-        },
-        ru: {
-            headerTop: {
-                title: 'SilverRain Израильская ювелирная компания с 2012 года. Все изделия из серебра произведены в Италии.',
-                login_title: 'Вход'
-            },
-            categories: {
-                rings: 'Кольца',
-                bracelets: 'Браслеты',
-                pendants: 'Подвески',
-                earrings: 'Серьги',
-                chokersNecklaces: 'Чокеры / Ожерелья',
-            },
-        },
-        ua: {
-            headerTop: {
-                title: 'Ювелірна компанія SilverRain Israel із 2012 року. Всі вироби зі срібла виготовлені в Італії',
-                login_title: 'Вхід',
-            },
-            categories: {
-                rings: 'Кільця',
-                bracelets: 'Браслети',
-                pendants: 'Підвіски',
-                earrings: 'Сережки',
-                chokersNecklaces: 'Кольє',
-            },
-        }
-    };
+    $('#lang_ru').on('click', function () {
+        $('body').addClass('lang_ru');
+        $('body').removeClass('lang_ua');
+        $('body').removeClass('lang_en');
+        $('.current-lang .lang-text').html($('#lang_ru p').html().toUpperCase());
+        $('.lang-dropdown #lang_ru').hide();
+        $('#lang_en').show();
+        $('#lang_ua').show();
+        $('.lang-dropdown').removeClass('active');
 
-    $('.header_notification p').html(translations[currLang].headerTop.title);
-    $('.login_title').html(translations[currLang].headerTop.login_title);
-    $('#ringsTestString').html(translations[currLang].categories.rings);
-    $('#braceletsTestString').html(translations[currLang].categories.bracelets);
-    $('#pedantsTestString').html(translations[currLang].categories.pendants);
-    $('#earringsTestString').html(translations[currLang].categories.earrings);
-    $('#chokersNecklacesTestString').html(translations[currLang].categories.chokersNecklaces);
+        let currLang = document.getElementsByTagName('body')[0].className.slice(-2);
+        console.log(currLang);
+
+        let translations = {
+            en: {
+                headerTop: {
+                    title: 'SilverRain Israel jewelry company since 2012. All silver products made in Italy',
+                    login_title: 'Login'
+                },
+                categories: {
+                    rings: 'Rings',
+                    bracelets: 'Bracelets',
+                    pendants: 'Pendants',
+                    earrings: 'Earrings',
+                    chokersNecklaces: 'Chokers / Necklaces',
+                },
+            },
+            ru: {
+                headerTop: {
+                    title: 'SilverRain Израильская ювелирная компания с 2012 года. Все изделия из серебра произведены в Италии.',
+                    login_title: 'Вход'
+                },
+                categories: {
+                    rings: 'Кольца',
+                    bracelets: 'Браслеты',
+                    pendants: 'Подвески',
+                    earrings: 'Серьги',
+                    chokersNecklaces: 'Чокеры / Ожерелья',
+                },
+            },
+            ua: {
+                headerTop: {
+                    title: 'Ювелірна компанія SilverRain Israel із 2012 року. Всі вироби зі срібла виготовлені в Італії',
+                    login_title: 'Вхід',
+                },
+                categories: {
+                    rings: 'Кільця',
+                    bracelets: 'Браслети',
+                    pendants: 'Підвіски',
+                    earrings: 'Сережки',
+                    chokersNecklaces: 'Кольє',
+                },
+            }
+        };
+
+        $('.header_notification p').html(translations[currLang].headerTop.title);
+        $('.login_title').html(translations[currLang].headerTop.login_title);
+        $('#ringsTestString').html(translations[currLang].categories.rings);
+        $('#braceletsTestString').html(translations[currLang].categories.bracelets);
+        $('#pedantsTestString').html(translations[currLang].categories.pendants);
+        $('#earringsTestString').html(translations[currLang].categories.earrings);
+        $('#chokersNecklacesTestString').html(translations[currLang].categories.chokersNecklaces);
+    });
+
+    $('#lang_ua').on('click', function () {
+        $('body').addClass('lang_ua');
+        $('body').removeClass('lang_ru');
+        $('body').removeClass('lang_en');
+        $('.current-lang .lang-text').html($('#lang_ua p').html().toUpperCase());
+        $('.lang-dropdown #lang_ua').hide();
+        $('#lang_en').show();
+        $('#lang_ru').show();
+        $('.lang-dropdown').removeClass('active');
+
+        let currLang = document.getElementsByTagName('body')[0].className.slice(-2);
+        console.log(currLang);
+
+        let translations = {
+            en: {
+                headerTop: {
+                    title: 'SilverRain Israel jewelry company since 2012. All silver products made in Italy',
+                    login_title: 'Login'
+                },
+                categories: {
+                    rings: 'Rings',
+                    bracelets: 'Bracelets',
+                    pendants: 'Pendants',
+                    earrings: 'Earrings',
+                    chokersNecklaces: 'Chokers / Necklaces',
+                },
+            },
+            ru: {
+                headerTop: {
+                    title: 'SilverRain Израильская ювелирная компания с 2012 года. Все изделия из серебра произведены в Италии.',
+                    login_title: 'Вход'
+                },
+                categories: {
+                    rings: 'Кольца',
+                    bracelets: 'Браслеты',
+                    pendants: 'Подвески',
+                    earrings: 'Серьги',
+                    chokersNecklaces: 'Чокеры / Ожерелья',
+                },
+            },
+            ua: {
+                headerTop: {
+                    title: 'Ювелірна компанія SilverRain Israel із 2012 року. Всі вироби зі срібла виготовлені в Італії',
+                    login_title: 'Вхід',
+                },
+                categories: {
+                    rings: 'Кільця',
+                    bracelets: 'Браслети',
+                    pendants: 'Підвіски',
+                    earrings: 'Сережки',
+                    chokersNecklaces: 'Кольє',
+                },
+            }
+        };
+
+        $('.header_notification p').html(translations[currLang].headerTop.title);
+        $('.login_title').html(translations[currLang].headerTop.login_title);
+        $('#ringsTestString').html(translations[currLang].categories.rings);
+        $('#braceletsTestString').html(translations[currLang].categories.bracelets);
+        $('#pedantsTestString').html(translations[currLang].categories.pendants);
+        $('#earringsTestString').html(translations[currLang].categories.earrings);
+        $('#chokersNecklacesTestString').html(translations[currLang].categories.chokersNecklaces);
+
+    });
+
+    $('#lang_en').on('click', function () {
+        $('body').addClass('lang_en');
+        $('body').removeClass('lang_ua');
+        $('body').removeClass('lang_ru');
+        $('.current-lang .lang-text').html($('#lang_en p').html().toUpperCase());
+        $('.lang-dropdown #lang_en').hide();
+        $('#lang_en').show();
+        $('.lang-dropdown').removeClass('active');
+
+        let currLang = document.getElementsByTagName('body')[0].className.slice(-2);
+        console.log(currLang);
+
+        let translations = {
+            en: {
+                headerTop: {
+                    title: 'SilverRain Israel jewelry company since 2012. All silver products made in Italy',
+                    login_title: 'Login'
+                },
+                categories: {
+                    rings: 'Rings',
+                    bracelets: 'Bracelets',
+                    pendants: 'Pendants',
+                    earrings: 'Earrings',
+                    chokersNecklaces: 'Chokers / Necklaces',
+                },
+            },
+            ru: {
+                headerTop: {
+                    title: 'SilverRain Израильская ювелирная компания с 2012 года. Все изделия из серебра произведены в Италии.',
+                    login_title: 'Вход'
+                },
+                categories: {
+                    rings: 'Кольца',
+                    bracelets: 'Браслеты',
+                    pendants: 'Подвески',
+                    earrings: 'Серьги',
+                    chokersNecklaces: 'Чокеры / Ожерелья',
+                },
+            },
+            ua: {
+                headerTop: {
+                    title: 'Ювелірна компанія SilverRain Israel із 2012 року. Всі вироби зі срібла виготовлені в Італії',
+                    login_title: 'Вхід',
+                },
+                categories: {
+                    rings: 'Кільця',
+                    bracelets: 'Браслети',
+                    pendants: 'Підвіски',
+                    earrings: 'Сережки',
+                    chokersNecklaces: 'Кольє',
+                },
+            }
+        };
+
+        $('.header_notification p').html(translations[currLang].headerTop.title);
+        $('.login_title').html(translations[currLang].headerTop.login_title);
+        $('#ringsTestString').html(translations[currLang].categories.rings);
+        $('#braceletsTestString').html(translations[currLang].categories.bracelets);
+        $('#pedantsTestString').html(translations[currLang].categories.pendants);
+        $('#earringsTestString').html(translations[currLang].categories.earrings);
+        $('#chokersNecklacesTestString').html(translations[currLang].categories.chokersNecklaces);
+
+    });
 };
 
-//langControl();
+langControl();
