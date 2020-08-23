eacaccd4d6de8e0861e7bd70432e6f0bcbc6dcaf
kor1k <koortttt@gmail.com>
2020-08-23 13:25:30 +0300

    changed pink color for lightblue and if items in category === 0 => show text about empty cat

--- src/assets/javascript/src/productsList/index.js
+++ src/assets/javascript/src/productsList/index.js
@@ -23,6 +23,13 @@ const productsList = () => {
                     document.querySelector('#productsPageItemsList').appendChild(productsListItem);
                 }
 
+                if ($('#productsPageItemsList .item').length === 0) {
+                    let noItems = document.createElement('div');
+                    noItems.className = 'article-level-4 text-center';
+                    noItems.innerText = 'Now there is no items for this category :(';
+
+                    document.querySelector('#productsPageItemsList').appendChild(noItems);
+                }
             }
 
             if (window.location.search.slice(-1) === '1') {
@@ -44,95 +51,129 @@ const productsList = () => {
                         document.querySelector('#productsPageItemsList').appendChild(productsListItem);
                     }
                 }
+
+                if ($('#productsPageItemsList .item').length === 0) {
+                    let noItems = document.createElement('div');
+                    noItems.className = 'article-level-4 text-center';
+                    noItems.innerText = 'Now there is no items for this category :(';
+
+                    document.querySelector('#productsPageItemsList').appendChild(noItems);
+                }
             }
-            //
-            // if (window.location.search.slice(-1) === '2') {
-            //     let catTitle = 'Bracelets';
-            //     let catTitleItem = document.createElement('div');
-            //     catTitleItem.innerHTML = `${catTitle}`;
-            //     document.querySelector('#catTitle').appendChild(catTitleItem);
-            //
-            //     for (let i = 0; i < testProductObject.data.length; i++) {
-            //         if (testProductObject.data[i].category_id === 2) {
-            //             const singleItemTestTitle = testProductObject.data[i].title;
-            //             const singleItemTestId = testProductObject.data[i].id;
-            //             const singleItemTestImg = testProductObject.data[i].images[i].url;
-            //
-            //
-            //             let productsListItem = document.createElement('div');
-            //             productsListItem.className = 'col-md-3 mb-20';
-            //             productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p>Price: ${testProductObject.data[i].current_price}<span class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
-            //
-            //             document.querySelector('#productsPageItemsList').appendChild(productsListItem);
-            //         }
-            //     }
-            // }
-            //
-            // if (window.location.search.slice(-1) === '3') {
-            //     let catTitle = 'Pendants';
-            //     let catTitleItem = document.createElement('div');
-            //     catTitleItem.innerHTML = `${catTitle}`;
-            //     document.querySelector('#catTitle').appendChild(catTitleItem);
-            //
-            //     for (let i = 0; i < testProductObject.data.length; i++) {
-            //         if (testProductObject.data[i].category_id === 3) {
-            //             const singleItemTestTitle = testProductObject.data[i].title;
-            //             const singleItemTestId = testProductObject.data[i].id;
-            //             const singleItemTestImg = testProductObject.data[i].images[i].url;
-            //
-            //
-            //             let productsListItem = document.createElement('div');
-            //             productsListItem.className = 'col-md-3 mb-20';
-            //             productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p>Price: ${testProductObject.data[i].current_price}<span class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
-            //
-            //             document.querySelector('#productsPageItemsList').appendChild(productsListItem);
-            //         }
-            //     }
-            // }
-            //
-            // if (window.location.search.slice(-1) === '4') {
-            //     let catTitle = 'Earrings';
-            //     let catTitleItem = document.createElement('div');
-            //     catTitleItem.innerHTML = `${catTitle}`;
-            //     document.querySelector('#catTitle').appendChild(catTitleItem);
-            //
-            //     for (let i = 0; i < testProductObject.data.length; i++) {
-            //         if (testProductObject.data[i].category_id === 4) {
-            //             const singleItemTestTitle = testProductObject.data[i].title;
-            //             const singleItemTestId = testProductObject.data[i].id;
-            //             const singleItemTestImg = testProductObject.data[i].images[i].url;
-            //
-            //
-            //             let productsListItem = document.createElement('div');
-            //             productsListItem.className = 'col-md-3 mb-20';
-            //             productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p>Price: ${testProductObject.data[i].current_price}<span class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
-            //
-            //             document.querySelector('#productsPageItemsList').appendChild(productsListItem);
-            //         }
-            //     }
-            // }
-            //
-            // if (window.location.search.slice(-1) === '5') {
-            //     let catTitle = 'Necklaces';
-            //     let catTitleItem = document.createElement('div');
-            //     catTitleItem.innerHTML = `${catTitle}`;
-            //     document.querySelector('#catTitle').appendChild(catTitleItem);
-            //
-            //     for (let i = 0; i < testProductObject.data.length; i++) {
-            //         if (testProductObject.data[i].category_id === 5) {
-            //             const singleItemTestTitle = testProductObject.data[i].title;
-            //             const singleItemTestId = testProductObject.data[i].id;
-            //             const singleItemTestImg = testProductObject.data[i].images[i].url;
-            //
-            //
-            //             let productsListItem = document.createElement('div');
-            //             productsListItem.className = 'col-md-3 mb-20';
-            //             productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p>Price: ${testProductObject.data[i].current_price}<span class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
-            //
-            //             document.querySelector('#productsPageItemsList').appendChild(productsListItem);
-            //         }
-            //     }
-            // }
+
+            if (window.location.search.slice(-1) === '2') {
+                let catTitle = 'Bracelets';
+                let catTitleItem = document.createElement('div');
+                catTitleItem.innerHTML = `${catTitle}`;
+                document.querySelector('#catTitle').appendChild(catTitleItem);
+
+                for (let i = 0; i < response.data.length; i++) {
+                    if (response.data[i].category_id === 2) {
+                        const singleItemTestTitle = response.data[i].title;
+                        const singleItemTestId = response.data[i].id;
+                        const singleItemTestImg = response.data[i].images[0].url;
+
+                        let productsListItem = document.createElement('div');
+                        productsListItem.className = 'col-md-3 mb-20';
+                        productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p style="font-weight: 500;">Price: ${response.data[i].current_price}<span style="margin-left: 5px;" class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
+
+                        document.querySelector('#productsPageItemsList').appendChild(productsListItem);
+                    }
+                }
+                if ($('#productsPageItemsList .item').length === 0) {
+                    let noItems = document.createElement('div');
+                    noItems.className = 'article-level-4 text-center';
+                    noItems.innerText = 'Now there is no items for this category :(';
+
+                    document.querySelector('#productsPageItemsList').appendChild(noItems);
+                }
+
+            }
+
+
+            if (window.location.search.slice(-1) === '3') {
+                let catTitle = 'Pendants';
+                let catTitleItem = document.createElement('div');
+                catTitleItem.innerHTML = `${catTitle}`;
+                document.querySelector('#catTitle').appendChild(catTitleItem);
+
+                for (let i = 0; i < response.data.length; i++) {
+                    if (response.data[i].category_id === 3) {
+                        const singleItemTestTitle = response.data[i].title;
+                        const singleItemTestId = response.data[i].id;
+                        const singleItemTestImg = response.data[i].images[0].url;
+
+                        let productsListItem = document.createElement('div');
+                        productsListItem.className = 'col-md-3 mb-20';
+                        productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p style="font-weight: 500;">Price: ${response.data[i].current_price}<span style="margin-left: 5px;" class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
+
+                        document.querySelector('#productsPageItemsList').appendChild(productsListItem);
+                    }
+                }
+                if ($('#productsPageItemsList .item').length === 0) {
+                    let noItems = document.createElement('div');
+                    noItems.className = 'article-level-4 text-center';
+                    noItems.innerText = 'Now there is no items for this category :(';
+
+                    document.querySelector('#productsPageItemsList').appendChild(noItems);
+                }
+            }
+
+            if (window.location.search.slice(-1) === '4') {
+                let catTitle = 'Earrings';
+                let catTitleItem = document.createElement('div');
+                catTitleItem.innerHTML = `${catTitle}`;
+                document.querySelector('#catTitle').appendChild(catTitleItem);
+
+                for (let i = 0; i < response.data.length; i++) {
+                    if (response.data[i].category_id === 4) {
+                        const singleItemTestTitle = response.data[i].title;
+                        const singleItemTestId = response.data[i].id;
+                        const singleItemTestImg = response.data[i].images[0].url;
+
+                        let productsListItem = document.createElement('div');
+                        productsListItem.className = 'col-md-3 mb-20';
+                        productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p style="font-weight: 500;">Price: ${response.data[i].current_price}<span style="margin-left: 5px;" class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
+
+                        document.querySelector('#productsPageItemsList').appendChild(productsListItem);
+                    }
+                }
+                if ($('#productsPageItemsList .item').length === 0) {
+                    let noItems = document.createElement('div');
+                    noItems.className = 'article-level-4 text-center';
+                    noItems.innerText = 'Now there is no items for this category :(';
+
+                    document.querySelector('#productsPageItemsList').appendChild(noItems);
+                }
+            }
+
+            if (window.location.search.slice(-1) === '5') {
+                let catTitle = 'Necklaces';
+                let catTitleItem = document.createElement('div');
+                catTitleItem.innerHTML = `${catTitle}`;
+                document.querySelector('#catTitle').appendChild(catTitleItem);
+
+                for (let i = 0; i < response.data.length; i++) {
+                    if (response.data[i].category_id === 5) {
+                        const singleItemTestTitle = response.data[i].title;
+                        const singleItemTestId = response.data[i].id;
+                        const singleItemTestImg = response.data[i].images[0].url;
+
+                        let productsListItem = document.createElement('div');
+                        productsListItem.className = 'col-md-3 mb-20';
+                        productsListItem.innerHTML = `<div class="item" id="item_id_${singleItemTestId}" style="background: url(${singleItemTestImg})  center center no-repeat; min-height: 250px; background-size: contain;"><div class="price_cta_preview"><a href="/product-page.html?=product_id_${singleItemTestId}">${singleItemTestTitle}</a><p style="font-weight: 500;">Price: ${response.data[i].current_price}<span style="margin-left: 5px;" class="currentCurrencyValPrice">${document.getElementById('currentCurrencyMain').innerHTML[0]}</span></p></div></div>`;
+
+                        document.querySelector('#productsPageItemsList').appendChild(productsListItem);
+                    }
+                }
+                if ($('#productsPageItemsList .item').length === 0) {
+                    let noItems = document.createElement('div');
+                    noItems.className = 'article-level-4 text-center';
+                    noItems.innerText = 'Now there is no items for this category :(';
+
+                    document.querySelector('#productsPageItemsList').appendChild(noItems);
+                }
+            }
         }
     });
 };
