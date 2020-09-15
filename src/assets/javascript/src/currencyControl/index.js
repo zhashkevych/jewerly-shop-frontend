67f85c129c4135ac011f81bf06679a2b19314898
korik <koortttt@gmail.com>
2020-09-15 16:24:28 +0300

    fixed currency cookies on macOS and iOS (now localStorage)

--- src/assets/javascript/src/currencyControl/index.js
+++ src/assets/javascript/src/currencyControl/index.js
@@ -27,18 +27,23 @@ const initCurrencySelector = (values) => {
 const initCurrencySelectorHandler = () => {
     $('#currencySelector').change(function () {
         let currency = $("#currencySelector option:checked").val();
-        setCookie(currencyCookieName, currency);
+        localStorage.setItem(currencyCookieName, currency)
+        // setCookie(currencyCookieName, currency);
         location.reload();
     });
 };
 
 const getCurrentCurrency = () => {
-    if (getCookie(currencyCookieName) === null) {
-        setCookie(currencyCookieName, defaultCurrency);
-        return defaultCurrency
+    if (localStorage.getItem(currencyCookieName) === null) {
+        localStorage.setItem(currencyCookieName, defaultCurrency)
     }
+    // if (getCookie(currencyCookieName) === null) {
+    //     setCookie(currencyCookieName, defaultCurrency);
+    //     return defaultCurrency
+    // }
 
-    return getCookie(currencyCookieName)
+    return localStorage.getItem(currencyCookieName)
+    // return getCookie(currencyCookieName)
 }
 
 const getCurrencyQueryParameter = () => {
