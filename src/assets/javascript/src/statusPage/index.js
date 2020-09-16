89df10d034e7c0206b1aa24e92cbde458f90c1d9
korik <koortttt@gmail.com>
2020-09-16 19:14:11 +0300

    statusPageHandling bugfix

--- src/assets/javascript/src/statusPage/index.js
+++ src/assets/javascript/src/statusPage/index.js
@@ -2,11 +2,18 @@ const statusPageHandling = () => {
     let successPage = document.querySelector('.response_success');
     let errorPage = document.querySelector('.response_error');
 
-    if (localStorage.getItem('success') === 'true') {
-        errorPage.classList.add('d-none');
-    } else {
-        successPage.classList.add('d-none');
+    if (successPage || errorPage) {
+        if (localStorage.getItem('success') === 'true') {
+            errorPage.classList.add('d-none');
+        } else {
+            successPage.classList.add('d-none');
+        }
     }
+
 }
 
 statusPageHandling();
+
+if (window.location.pathname === '/status-page.html') {
+    statusPageHandling();
+}
